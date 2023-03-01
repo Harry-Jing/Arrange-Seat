af56485b6196f829cbc66c4451c100420bccc566
487457439 <117627990+487457439@users.noreply.github.com>
2023-03-01 21:22:50 +0800

    3.1 (#3)

--- randomSeat/utils.py
+++ randomSeat/utils.py
@@ -2,6 +2,8 @@ from typing import Optional, Generator
 
 from .dataStruct import *
 
+from random import shuffle
+
 
 class Seat:
     def __init__(self) -> None:
@@ -38,12 +40,16 @@ class SeatingChart:
         for i in seat_format.split('\n'):
             seat_chart.append([Aisle() if j == ' ' else Seat() for j in i])
         self.chart: list[list[Seat | Aisle]] = seat_chart
+        self.seats: list[list[Seat]] = self.removeAisle()
 
         # 初始化座位图的行列长度
         self.row_len = len(self.chart[0])
         self.line_len = len(self.chart)
         # ToDo:检测座位图是否合法(必须是矩形)
         pass
+
+        self.stu_list = []
+        self._loadStu(config)
 
     def __repr__(self) -> str:
         return f'<SeatingChart(...)>'
@@ -78,6 +84,25 @@ class SeatingChart:
             return f'[ {name} ]'
         else:
             return f'[{name[0:3]}]'
+
+    def _loadStu(self, config) -> None:
+        for people in config['people']:
+            self.stu_list.append(Person(**people))
+
+    def randomize(self) -> None:
+        shuffle(self.stu_list)
+        for line in self.seats:
+            for seat in line:
+                seat.seater = self.stu_list.pop(0)
+
+    def removeAisle(self) -> list[list[Seat]]:
+        seats = []
+        for line in self.chart:
+            for seat in line:
+                if isinstance(seat, Aisle):
+                    line.remove(seat)
+            seats.append(line)
+        return seats
 
     @property
     def row(self) -> list[Seat | Aisle]:
